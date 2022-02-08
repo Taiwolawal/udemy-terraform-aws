8dbed557856d07453a144ed9b9991ce17b156e16
Taiwo Lawal <taiwolawal360@gmail.com>
2022-02-08 20:50:49 +0100

    deploy

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 variable avail_zone {}
 variable vpc_cidr_block {}
-variable subnet_cidr_block {}
+variable subnet_cidr_block {}   
 variable env_prefix {}
 variable my_ip {}
 variable instance_type{}
